f5c0495a2d7c6a1156cf5e02ae35d33090b2dd72
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-05-05 15:26:12 +0200

    Nodjes

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
 
 var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + 'public'));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 require('./app/routes')(app);
 
 app.listen(port);
 
 console.log('Node.js server has started on port ' + port);
+
+exports = module.exports = app;
